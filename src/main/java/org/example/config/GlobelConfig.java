440d3cb889fd6bf2909f4b80c5262c2f1efb71b7
lianghuang6 <lianghuang6@iflytek.com>
2020-12-30 17:58:02 +0800

    11

--- src/main/java/org/example/config/GlobelConfig.java
+++ src/main/java/org/example/config/GlobelConfig.java
@@ -1,11 +1,14 @@
 package org.example.config;
 
 import java.awt.*;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class GlobelConfig {
 
     //开关控制
     public static volatile Boolean SWITCHFLAG = false;
+
+    public static volatile ReentrantLock LOCK = new ReentrantLock();
 
     //机器人实体
     public static Robot robot;
